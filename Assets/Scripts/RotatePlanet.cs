31bd495aae9d585f9066231dcd80dd80114b1373
Bekir Evrim Sümer <bekirevrimsumer@gmail.com>
2022-07-02 10:45:07 +0300

    Print to log when planet completes one revolution

--- Assets/Scripts/RotatePlanet.cs
+++ Assets/Scripts/RotatePlanet.cs
@@ -7,6 +7,8 @@ public class RotatePlanet : MonoBehaviour
     public PlanetData planetData;
     public GameObject sun;
 
+    private float rotationAngle;
+
     private void Start()
     {
         sun = GameObject.Find("Sun");
@@ -20,6 +22,13 @@ public class RotatePlanet : MonoBehaviour
         //planets orbiting the sun
         transform.RotateAround(sun.transform.position, new Vector3(0, 1, 0), planetData.planetRotateSpeed * Time.deltaTime);
 
+        //print to log when planet completes one revolution
+        rotationAngle += Time.deltaTime * planetData.planetRotateSpeed;
+        if (rotationAngle >= 360)
+        {
+            Debug.Log($"{planetData.name} has completed one revolution");
+            rotationAngle = 0;
+        }
 
     }
 }
